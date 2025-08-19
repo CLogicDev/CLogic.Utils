8224b1c619af9c837c68aefddb7bbb57332edfeb
Shaun Maurer Masson <maurermasson.shaun@icloud.com>
2025-08-19 08:25:07 +0400

    Unity Data Saving Base Import

---
+++ Assets/CLogic.Utils/Services.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CLogic.Core.LifeCycles;
+using CLogic.Core.Services;
+using UnityEngine;
+namespace CLogic.Utils
+{
+    public static class Services
+    {
+        static ServiceManager manager = new();
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void SetupManager()
+        {
+            manager?.Dispose();
+
+            manager = new ServiceManager();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void SetupUpdater()
+        {
+            GameObject updater = new("Service Updater");
+            updater.AddComponent<ServiceUpdater>();
+            updater.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        public static void Resolve<T>(LifeCycle lifeCycle) => manager.Resolve<T>(lifeCycle);
+        
+        public static void Register<T>(T instance, LifeCycle lifeCycle) where T : IDisposable => manager.Register(instance, lifeCycle);
+        
+        public static void HasService<T>(LifeCycle lifeCycle) where T : IDisposable => manager.HasService<T>(lifeCycle);
+
+        public static void DeRegister<T>(LifeCycle lifeCycle) => manager.DeRegister<T>(lifeCycle);
+        
+        public static void EndLifeCycle(LifeCycle lifeCycle) => manager.EndLifeCycle(lifeCycle);
+
+        public static void CheckLifeCycles() => manager.CheckLifeCycles();
+        
+        public static void CheckLifeCycle(LifeCycle lifeCycle) => manager.CheckLifeCycle(lifeCycle);
+    }
+
+    public class ServiceUpdater : MonoBehaviour
+    {
+        private void Update()
+        {
+            Services.CheckLifeCycles();
+        }
+    }
+}
