cfb31fc27595582cf89c9ddc9ee1ea19572619b8
Shaun Maurer Masson <maurermasson.shaun@icloud.com>
2025-09-16 23:30:02 +0400

    Slot Provider Class

---
+++ Assets/CLogic.Utils/Runtime/Data Saving/SlotProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CLogic.Utils.DataSaving;
+using UnityEngine;
+namespace CLogic.Utils.Runtime.DataSaving
+{
+    public class SlotProvider : MonoBehaviour, IDisposable
+    {
+        public string slotId;
+
+        private void Awake()
+        {
+            if(Services.HasService<SlotProvider>(SceneLifeCycle.Instance))
+                return;
+            
+            Services.Register(this, SceneLifeCycle.Instance);
+            
+            Slotter.CurrentSlotId = slotId;
+        }
+        public void Dispose()
+        {
+            Destroy(this);
+        }
+    }
+}
