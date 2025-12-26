899d05c93f1297b9491958a5969984a09d7cdf8b
Shaun Maurer Masson <shaun@clogic.dev>
2025-12-26 15:48:43 +0400

    Delayed Calls
    Signed-off-by: Shaun Maurer Masson <shaun@clogic.dev>

---
+++ Assets/CLogic.Utils/Tests/DelayTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+namespace CLogic.Utils.Tests
+{
+    public class DelayTest : MonoBehaviour
+    {
+        public float delay;
+
+        [ContextMenu("AddDelay")]
+        public void CallDelay()
+        {
+            float c = delay;
+            
+            StaticUtils.ExecuteDelayed(() => Debug.Log("Delayed by " + c), delay);
+            Debug.Log($"Delay: {delay}");
+        }
+    }
+}
