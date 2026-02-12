92d6cb44c066aeef3aa64e8cdae824712f33a805
Shaun Maurer Masson <shaun@clogic.dev>
2026-02-12 12:29:32 +0400

    feat: Added player loop utility

---
+++ Assets/CLogic.Utils/Tests/PlayerLoopTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CLogic.Utils.Shared;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+namespace CLogic.Utils.Tests
+{
+    public class PlayerLoopTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            PlayerLoopInterface.InsertSystemBefore(typeof(PlayerLoopTest), UpdateFunction, typeof(Update.ScriptRunBehaviourUpdate));
+        }
+
+        void UpdateFunction()
+        {
+            Debug.Log("frame "  + Time.frameCount);
+        }
+    }
+}
